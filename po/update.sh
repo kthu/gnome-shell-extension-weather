6b778a0048ad4325139800582f648503049c0ddd
Canek Peláez <canek@ciencias.unam.mx>
2013-02-02 00:08:16 -0600

    po: Update the i18n infrastructure. All the po files have been updated and are updated from the source code using the update.sh script. I will try to fix that so that update-po actually works.

--- po/update.sh
+++ po/update.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-POT=gnome-shell-extension-weather.pot 
-TMP=messages.po
-TMP2=messages2.po
+POT=gnome-shell-extension-weather.pot
+SCH_IN=../data/org.gnome.shell.extensions.weather.gschema.xml.in
+SCH_H=../data/org.gnome.shell.extensions.weather.gschema.xml.in.h
+MSG=messages.po
+TMP=merged.po
 
 # update pot
-echo '' > $TMP
-pushd ../src
-xgettext --no-location -o ../po/$TMP -L js -j --keyword=_ ../src/*.js ../data/*.xml
-popd
-msgmerge -N $POT $TMP > $TMP2
-mv $TMP2 $POT
-rm -f $TMP $TMP2
+make ${POT}
+echo '' > ${MSG}
+intltool-extract --type=gettext/xml "${SCH_IN}"
+xgettext -c --from-code UTF-8 -o ${MSG} -L python -j --keyword=_ ../src/*.js
+xgettext -c --from-code UTF-8 -o ${MSG} -L glade  -j --keyword=_ ../data/*.xml
+xgettext --no-location --from-code UTF-8 -o ${MSG} -L C -j --keyword=N_ ${SCH_H}
+msgmerge -N ${POT} ${MSG} > ${TMP}
+mv -f ${TMP} ${POT}
+rm -f ${MSG} ${SCH_H}
 
 # update po's
-for po in *po; do msgmerge -U $po gnome-shell-extension-weather.pot; done
+for po in $(cat LINGUAS); do
+    echo -n "Updating ${po}: "
+    msgmerge -U "${po}.po" gnome-shell-extension-weather.pot;
+done
